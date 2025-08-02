8596328c1bc5efc30dd8bac6a554870aab208e44
SOUVIK SAHA <souvik21102001@gmail.com>
2025-08-02 19:09:08 +0530

    Programme 5

---
+++ day2/p-1.py
@@ -0,0 +1,20 @@
+# String operations
+
+name = "Souvik Saha"
+
+print(name.upper())
+print(name.lower())
+
+print(name)
+
+
+print(name.find('S'))
+print(name.find('B'))
+print(name.find("saha"))
+print(name.find("Saha"))
+
+print(name.replace("Saha", "Roy"))
+print(name)
+
+print("Saha" in name)
+print("saha" in name)
