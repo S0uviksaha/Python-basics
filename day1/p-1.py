947967f85568e8719547f624fb5ffb77880cad80
SOUVIK SAHA <souvik21102001@gmail.com>
2025-08-02 19:08:30 +0530

    Programme 2

---
+++ day1/p-1.py
@@ -0,0 +1,10 @@
+# Variable declaration and printing
+
+firstName = "Tony"
+lastName = "Stark"
+age = 51
+isGenius = True
+
+print(firstName + " " + lastName)
+print("Age:", age)
+print("Is Genius:", isGenius)
