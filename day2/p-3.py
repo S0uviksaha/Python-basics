a00e453c7390f212a3081ea5ad54f64442ce455d
SOUVIK SAHA <souvik21102001@gmail.com>
2025-08-02 19:11:46 +0530

    Programme 7

---
+++ day2/p-3.py
@@ -0,0 +1,24 @@
+# Shortened code for arithmetic operations with assignment operators
+
+i = 4
+i += 2  # i = i + 2
+print(i)  # 6
+
+i -= 2  # i = i - 2
+print(i)  # 4
+
+i *= 2  # i = i * 2
+print(i)  # 8
+
+i /= 2  # i = i / 2
+print(i)  # 4.0
+
+i //= 2  # i = i // 2
+print(i)  # 2.0
+
+i **= 2  # i = i ** 2
+print(i)  # 4.0
+
+i %= 2  # i = i % 2
+print(i)  # 0.0
+
