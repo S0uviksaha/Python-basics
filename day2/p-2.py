aa8b27be2bdb59d406ad1a4964ffeeef199dcd83
SOUVIK SAHA <souvik21102001@gmail.com>
2025-08-02 19:09:22 +0530

    Programme 6

---
+++ day2/p-2.py
@@ -0,0 +1,10 @@
+# Arithmetic operations in Python
+
+print(5*2) # 10
+print(5/2) # 2.5    
+print(5//2) # 2 only integer part
+print(5%2) # 1 remainder    
+print(5**2) # 25 power of 2
+print(5+2) # 7  
+print(5-2) # 3
+
