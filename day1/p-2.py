060cbc138ec72f72d4fdf96d34873bea01fa97c5
SOUVIK SAHA <souvik21102001@gmail.com>
2025-08-02 19:08:39 +0530

    Programme 3

---
+++ day1/p-2.py
@@ -0,0 +1,7 @@
+# input function to get user input
+
+superHero = input("Enter your superhero name: ")
+print("Your superhero name is:", superHero)
+
+name = input("Enter your name: ")
+print("Superhero of" + " " + name + " is " + superHero)
