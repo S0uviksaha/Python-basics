9e88fd8fbbdfe844e1e9ca98a9517923e5cef5e4
SOUVIK SAHA <souvik21102001@gmail.com>
2025-08-04 09:34:19 +0530

    programme 13

---
+++ day5/p-1.py
@@ -0,0 +1,18 @@
+# list 
+
+marks = [10, 20, 30, 40, 50]
+print(marks)
+
+# for loop
+for i in range(len(marks)):
+    print(marks[i])  # Print each mark
+
+# while loop
+i = 0
+while i < len(marks):
+    print(marks[i])  # Print each mark
+    i += 1  # Increment i by 1 each iteration
+
+
+
+
