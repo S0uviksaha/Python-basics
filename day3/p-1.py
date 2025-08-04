2527f89f8cc1758c7967c383436c0f0990f680c0
SOUVIK SAHA <souvik21102001@gmail.com>
2025-08-04 09:31:57 +0530

    programme 10

---
+++ day3/p-1.py
@@ -0,0 +1,13 @@
+# if-else statement
+
+age = 20
+
+if age >= 18:
+    print("You are eligible to vote.")
+elif age >= 13:
+    print("You are a teenager.")    
+else:
+    print("You are a child.")
+
+print("End of program.")
+
