bd453113feda75e60e6086778f9ffb3f7d48b4f1
SOUVIK SAHA <souvik21102001@gmail.com>
2025-08-04 09:32:25 +0530

    programme 12

---
+++ day4/p-2.py
@@ -0,0 +1,16 @@
+# pattern 
+
+i = 0
+while i < 5:
+    print("*" * (i + 1))  # Print '*' i+1 times
+    i += 1  # Increment i by 1 each iteration
+
+
+i = 5
+
+while i > 0:
+    print("*" * i)  # Print '*' i times
+    i -= 1  # Decrement i by 1 each iteration
+
+
+
