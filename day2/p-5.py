aa050d62b86a6b5683c34fdc2d7650c0eff4f522
SOUVIK SAHA <souvik21102001@gmail.com>
2025-08-04 09:31:39 +0530

    programme 9

---
+++ day2/p-5.py
@@ -0,0 +1,10 @@
+# Logical Operators
+
+print (2 > 1 and 3 > 2)  # True
+print (2 > 1 and 3 < 2)  # False
+
+print (2 > 1 or 3 < 2)   # True
+print (2 < 1 or 3 < 2)   # False
+
+print (not (2 > 1))      # False
+print (not (2 < 1))      # True
