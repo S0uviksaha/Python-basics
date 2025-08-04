49fbf043efb36e87d79894166754b018302ae7cb
SOUVIK SAHA <souvik21102001@gmail.com>
2025-08-04 09:34:56 +0530

    programme 16

---
+++ day5/p-4.py
@@ -0,0 +1,10 @@
+# negetive indexing
+
+my_list = [10, 20, 30, 40, 50]
+print(my_list[-1])  # Access the last element (50)
+print(my_list[-2])  # Access the second last element (40)
+print(my_list[-3])  # Access the third last element (30)
+
+
+# slicing 
+print(my_list[0:3])  # Access elements from index 0 to 2 (10, 20, 30)
