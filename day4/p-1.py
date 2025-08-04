9c9c0e1080411c432548a1863397b3c6f8a090ef
SOUVIK SAHA <souvik21102001@gmail.com>
2025-08-04 09:32:13 +0530

    programme 11

---
+++ day4/p-1.py
@@ -0,0 +1,17 @@
+# while loop
+
+i = 0
+while i < 5:
+    print(i)
+    i += 1  # Increment i by 1 each iteration
+
+print("Loop finished ")
+
+
+
+i = 9
+while i > 0:
+    print(i)
+    i -= 1  # Decrement i by 1 each iteration
+
+print("Loop finished")
